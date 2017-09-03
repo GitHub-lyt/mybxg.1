6006740d2008866f2047cf50c1a6251d428a0c94
liuyuntao <aaa@qq.com>
2017-09-03 17:18:51 +0800

    完成头像上传功能

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,4 +1,4 @@
-define(['jquery','template','util'],function ($,template,util){
+define(['jquery','template','util','datepicker','language','uploadify'],function ($,template,util){
     //设置导航菜单选中
     util.setMenu('/main/index');
     //调用后台接口获取所有个人数据
@@ -7,9 +7,25 @@ define(['jquery','template','util'],function ($,template,util){
         url : '/api/teacher/profile',
         dataType : 'json',
         success : function (data) {
-            //解析数据渲染页面
-            var html = template('settingsTpl',data.result);
-            $('#settingsInfo').html(html);
+        //解析数据渲染页面
+        var html = template('settingsTpl',data.result);
+    $('#settingsInfo').html(html);
+    //处理头像上传
+    $('#upfile').uploadify({
+        width : 120,
+        height : 120,
+        buttonText : '',
+        itemTemplate : '<span></span>',
+        fileObjName : 'tc_avatar',
+        swf : '/public/assets/uploadify/uploadify.swf',
+        uploader : '/api/uploader/avatar',
+        onUploadSuccess : function (f,data) {
+                var data = JSON.parse(data);
+                 //修改图片的URL地址
+                // console.log($('.preview img').size());
+                $('.preview img').attr('src',data.result.path);
+                }
+            });
         }
     });
 });
